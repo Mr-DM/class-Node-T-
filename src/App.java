2a5b986cff773d11febcc5494acbc5d195b9d41e
Mr-Dm <danm306009@gmail.com>
2026-02-12 11:33:55 +0200

    thired commit

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Node<Character> node1 = new Node<>('a');
-        
-        
-        
+        node1.addlast('b');
+        node1.addlast('c');
+        node1.addlast('d');
+        node1.PrintValues();
     }
+
+
 }
--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.Node;
+
 public class Node<T> {
     private T _value;
     private Node<T> _next;
@@ -20,13 +22,26 @@ public class Node<T> {
     public boolean HasNext(){
         return _next != null;
     }
+
     public void PrintValues(){
         Node<T> p = this;
         while (p != null) {
-            System.out.println(p.getValue());
+            System.out.print(p.getValue() + " -> ");
             p = p.getNext();
         }
+        System.out.println("null");
     } 
+
+    public void addlast(T value){
+        Node<T> p = this;
+        while (p.getNext() != null) {
+            p = p.getNext();
+        }
+        p.setNext(new Node<>(value));
+    }
+
+
+    
 
     // int - > Integer
     // double -> Double
